26e0d8b591bdaae9b7740656fa04d50a5e9f75d6
Tim Simms <tsimms@gmail.com>
2023-04-09 11:13:11 -0500

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,43 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import logo from './holberton-logo.png';
+
+
+const people = [
+  {
+    name: 'Tim Simms',
+    github: 'https://github.com/TimSimms84',
+    linkedin: 'https://www.linkedin.com/in/timcsimms/',
+  },
+  {
+    name: 'Trenton Sims',
+    github: 'https://github.com/trentonsims',
+    linkedin: 'https://www.linkedin.com/in/tsims115/',
+  },
+  {
+    name: 'Blake Stewart',
+    github: 'https://github.com/Acochisse',
+    linkedin: 'https://www.linkedin.com/in/leestewart909/',
+  },
+];
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="App">
+        <h1>FIND US HERE</h1>
+        <div className="links-container">
+          {people.map((person, index) => (
+            <div key={index} className="person">
+              <h2>{person.name}</h2>
+              <a href={person.github} target="_blank" rel="noopener noreferrer">GitHub</a>
+              <a href={person.linkedin} target="_blank" rel="noopener noreferrer">LinkedIn</a>
+            </div>
+          ))}
+        </div>
+      </div>
+      <img src={logo} alt="Holberton School Logo" className="holberton-logo" />
+    </>
   );
 }
 
